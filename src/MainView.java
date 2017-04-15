4c62e23811f7d7158f69f010621b3cb6b4b37f83
Aaron Morris <amorris2@ou.edu>
2017-04-15 13:25:23 -0500

    Add empty Restaurant and add activate view method. Just createdthe frame and an unused JList called leftList

--- src/MainView.java
+++ src/MainView.java
@@ -1,9 +1,24 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.*;
 
 public class MainView extends JFrame
 {
+	JList<Restaurant> leftList;
 	public MainView()
 	{
-		
+		leftList = new JList<Restaurant>();
+	}
+	
+	public void activateView()
+	{
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = Math.round(screenSize.getWidth());
+		double height = Math.round(screenSize.getHeight());
+		/*Set the size of the frame to the screen size*/
+		setSize((int) width, (int) height);
+		setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 	}
 }

7620d24fe6017c4dae0c10b3b85c5c10b4212fe2
Aaron Morris <amorris2@ou.edu>
2017-04-29 12:07:34 -0500

    updating my code

--- src/Driver.java
+++ src/Driver.java
@@ -3,14 +3,15 @@ public class Driver
 {
 	public static void main(String[] args)
 	{
-		WelcomeView welcome = new WelcomeView();
-		LoadingView loading = new LoadingView();
-		Controller controller = new Controller();
-		MainView mv = new MainView();
-		controller.setWelcomeView(welcome);
-		controller.setLoadingView(loading);
-		controller.activateWelcomeViews();
-		controller.setMainView(mv);
-		
+//		WelcomeView welcome = new WelcomeView();
+//		LoadingView loading = new LoadingView();
+//		Controller controller = new Controller();
+//		MainView mv = new MainView();
+//		controller.setWelcomeView(welcome);
+//		controller.setLoadingView(loading);
+//		controller.activateWelcomeViews();
+//		controller.setMainView(mv);
+		MainView test = new MainView();
+		test.activateView();
 	}
 }
